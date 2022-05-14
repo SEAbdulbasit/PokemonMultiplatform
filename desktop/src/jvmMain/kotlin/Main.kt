68c0d61146692c7b7ff2069c948026ae4bdd04a2
Abdul Basit <abdulbasit5361234@gmail.com>
2022-05-14 16:41:02 +0500

    Dependencies added

---
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -0,0 +1,32 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import androidx.compose.foundation.LocalScrollbarStyle
+import androidx.compose.foundation.ScrollbarStyle
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        Window(
+            onCloseRequest = ::exitApplication
+        ) {
+            MaterialTheme {
+                CompositionLocalProvider(
+                    LocalScrollbarStyle provides ScrollbarStyle(
+                        minimalHeight = 16.dp,
+                        thickness = 8.dp,
+                        shape = MaterialTheme.shapes.small,
+                        hoverDurationMillis = 300,
+                        unhoverColor = MaterialTheme.colors.onSurface.copy(alpha = 0.12f),
+                        hoverColor = MaterialTheme.colors.onSurface.copy(alpha = 0.50f)
+                    )
+                ) {
+
+                }
+            }
+        }
+    }
+}
